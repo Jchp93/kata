0335a6518c935f79a86f9f25f09c245e208c8d3d
Jchp93 <jchp93@yahoo.fr>
2024-04-24 09:37:43 +0200

    Creation de JS avec Fizz Buzz

---
+++ script.js
@@ -0,0 +1,24 @@
+let nombreMax = 100;
+
+let liste = "";
+
+for (let i = 0; i <=nombreMax; i ++) {
+    if (i % 3 == 0) {
+        liste += "Fizz";
+        liste += "\n";
+    }
+    if (i % 5 == 0) {
+        liste += "Buzz";
+        liste += "\n";
+    }
+    if (i % 3 == 0 && i % 5 == 0) {
+         liste += "Fizz Buzz";
+         liste += "\n";
+     }
+    else {
+        liste += i;
+        liste += "\n";
+    }  
+}
+  console.log(liste);
+
